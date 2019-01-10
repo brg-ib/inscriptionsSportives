ed954c2a675ab2a987f01049405bd1e7679aa92e
brg-ib <ihcen@me.com>
2019-01-10 16:33:00 +0100

    Test Unitaire setPrenom/setNom

--- InscriptionsSportives/src/TestJunit/InscriptionsTest.java
+++ InscriptionsSportives/src/TestJunit/InscriptionsTest.java
@@ -1,5 +1,6 @@
 package TestJunit;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -8,16 +9,44 @@ import inscriptions.Inscriptions;
 import inscriptions.Personne;
 
 public class InscriptionsTest {
-
 	
 	@Test
-	public void testCreatePersonne() {
+	public void setUp() throws Exception {
 	Inscriptions inscr = Inscriptions.getInscriptions();
-	Personne Brahim = inscr.createPersonne("Brahim", "Mlaghui", "bra@gmail.com"),
-			Ihcen = inscr.createPersonne("Ihcen", "Borgi", "ihcen@me.com");
+	Personne Brahim = inscr.createPersonne("Mlaghui", "Brahim", "bra@gmail.com"),
+			Ihcen = inscr.createPersonne("Borgi", "Ihcen", "ihcen@me.com");
+
+	assertTrue(Brahim.getPrenom(), Brahim.getPrenom().equals("Brahim"));
+	assertTrue(Brahim.getNom(), Brahim.getNom().equals("Mlaghui"));
+	assertTrue(Brahim.getMail(), Brahim.getMail().equals("bra@gmail.com"));
 	
-	assertTrue(Brahim.getNom(), Ihcen.getNom().equals("Ihcen"));
+	Brahim.setPrenom("Dany");
+	Ihcen.setPrenom("Philippe");
+	assertEquals("Dany", Brahim.getPrenom());
+	assertEquals("Philippe", Ihcen.getPrenom());
+	
+	Brahim.setNom("Boon");
+	Ihcen.setNom("Auguste");
+	assertEquals("Boon", Brahim.getNom());
+	assertEquals("Auguste", Ihcen.getNom());
+	
 	
 	}
+	
+	@Test
+	public void testSetPrenom() {
+
+	}
+	
+	@Test
+	public void testSetNom() {
+	
+	}
+	
+	@Test
+	public void testSetMail() {
+	
+	}
+	
 	
 }
