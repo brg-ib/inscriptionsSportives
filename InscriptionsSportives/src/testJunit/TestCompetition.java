fc7b43174dd7eaa70e9dbccda85fa821d6802abb
brg-ib <ihcen@me.com>
2019-01-10 19:26:24 +0100

    Ajoute testCompetition

--- InscriptionsSportives/src/testJunit/TestCompetition.java
+++ InscriptionsSportives/src/testJunit/TestCompetition.java
@@ -14,16 +14,27 @@ import inscriptions.Personne;
 
 public class TestCompetition {
 
+	private LocalDate datecloture = LocalDate.of(2021, 11, 11);
+	private LocalDate datecloture2 = LocalDate.of(2018, 04, 29);
+	
+	@Test
+	public void setUp() throws Exception {
+	
 	Inscriptions inscr = Inscriptions.getInscriptions();
 
-	LocalDate dateclôture = LocalDate.of(2021, 11, 11);
-	LocalDate dateclôture2 = LocalDate.of(2019, 04, 29);
-	LocalDate dateclôture3 = LocalDate.of(2012, 12, 31);
-
-	Competition c1 = inscr.createCompetition("Poker", dateclôture, false);
-	Competition c2 = inscr.createCompetition("Billard", dateclôture2, true);
-	
+	Competition c1 = inscr.createCompetition("Poker", datecloture, false);
+	Competition c2 = inscr.createCompetition("Billard", datecloture2, true);
 	
 	Personne p1 = inscr.createPersonne("Robert","DeNiro","deniro.Robert@youtalkingtome.com");
+	Personne p2 = inscr.createPersonne("Marlon","Brando","marlon.brando@thgodfhater.com");
+
 	Equipe e1 = inscr.createEquipe("Gangs");
+	Equipe e2 = inscr.createEquipe("Bulls");
+	
+	assertEquals("Poker", c1.getNom());
+	assertEquals("Billard", c2.getNom());
+	assertTrue(c1.inscriptionsOuvertes());
+	assertTrue(c2.inscriptionsOuvertes());
+	
+	}
 }
