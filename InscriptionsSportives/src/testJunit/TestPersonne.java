aea819f09287d29b1d8fe4480f2ac10f075bdeda
brg-ib <ihcen@me.com>
2019-01-14 11:54:38 +0100

    Modifications des tests

--- InscriptionsSportives/src/testJunit/TestPersonne.java
+++ InscriptionsSportives/src/testJunit/TestPersonne.java
@@ -2,40 +2,74 @@ package testJunit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
-import org.junit.Test;
+import java.util.*;
 
+import org.junit.*;
+
+import inscriptions.Equipe;
 import inscriptions.Inscriptions;
 import inscriptions.Personne;
 
 public class TestPersonne {
 	
-	@Test
+	private Inscriptions inscr = Inscriptions.getInscriptions();; 
+	private Personne personne;
+
+	
+	@Before // avec cette annotation, cette méthode sera appelée avant chaque test
 	public void setUp() throws Exception {
-	Inscriptions inscr = Inscriptions.getInscriptions();
-	Personne Brahim = inscr.createPersonne("Mlaghui", "Brahim", "bra@gmail.com"),
-			Ihcen = inscr.createPersonne("Borgi", "Ihcen", "ihcen@me.com");
-
-	assertTrue(Brahim.getPrenom(), Brahim.getPrenom().equals("Brahim"));
-	assertTrue(Brahim.getNom(), Brahim.getNom().equals("Mlaghui"));
-	assertTrue(Brahim.getMail(), Brahim.getMail().equals("bra@gmail.com"));
-	
-	Brahim.setPrenom("Dany");
-	Ihcen.setPrenom("Philippe");
-	assertEquals("Dany", Brahim.getPrenom());
-	assertEquals("Philippe", Ihcen.getPrenom());
-	
-	Brahim.setNom("Boon");
-	Ihcen.setNom("Auguste");
-	assertEquals("Boon", Brahim.getNom());
-	assertEquals("Auguste", Ihcen.getNom());
-	
-	Brahim.setMail("danyboon@gmail.com");
-	Ihcen.setMail("philippeauguste@me.com");
-	assertEquals("danyboon@gmail.com", Brahim.getMail());
-	assertEquals("philippeauguste@me.com", Ihcen.getMail());
-
-	
+		 personne = inscr.createPersonne("Mlaghui", "Brahim", "bra@gmail.com");
 	}
 	
+	@Test
+	public void testGetPrenom() {
+		assertEquals("Brahim", personne.getPrenom());	
+	}
+	
+	@Test
+	public void testSetPrenom() {
+		personne.setPrenom("Dany");
+		assertEquals("Dany", personne.getPrenom());
+	}
+	
+	@Test
+	public void testSetNom() {
+		personne.setNom("Boon");
+		assertEquals("Boon", personne.getNom());
+	}
+
+	@Test
+	public void testGetNom() {
+		assertTrue(personne.getNom(), personne.getNom().equals("Mlaghui"));
+	}
+	
+	@Test
+	public void testSetMail() {
+		personne.setMail("danyboon@gmail.com");
+		assertEquals("danyboon@gmail.com", personne.getMail());
+	}
+	
+	@Test
+	public void testGetMail() {
+		assertTrue(personne.getMail(), personne.getMail().equals("bra@gmail.com"));
+	}
+	
+	@Test
+	public void testGetEquipe() {
+		Equipe equipe1 = inscr.createEquipe("Equipe de Poker");
+		equipe1.add(personne);
+		Set<Equipe> setEquipe1 = personne.getEquipes();
+		assertTrue(setEquipe1.contains(equipe1));
+	}
+	@Test
+	public void testGetPersonneEquipe() {
+		Equipe equipe1 = inscr.createEquipe("Equipe de Poker");
+		equipe1.add(personne);
+		Set<Equipe> setEquipe1 = personne.getEquipes();
+		personne.delete();
+		assertFalse(setEquipe1.contains(equipe1));
+	}
+
 }
