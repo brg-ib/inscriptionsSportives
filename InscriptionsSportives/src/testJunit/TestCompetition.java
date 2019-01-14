1e7150fec2269a666e8d6fdbacb19d638f16ef73
brg-ib <ihcen@me.com>
2019-01-14 12:47:33 +0100

    Amelioration des test

--- InscriptionsSportives/src/testJunit/TestCompetition.java
+++ InscriptionsSportives/src/testJunit/TestCompetition.java
@@ -13,41 +13,62 @@ import inscriptions.Personne;
 
 public class TestCompetition {
 
+	private Inscriptions inscr = Inscriptions.getInscriptions();
+	private Personne p1,p2;
+	private Competition c1, c2;
+	private Equipe e1, e2;
 	private LocalDate datecloture = LocalDate.of(2021, 11, 11);
 	private LocalDate datecloture2 = LocalDate.of(2018, 04, 29);
 	
+	@Before
+	public void setUp() throws Exception {		
+		
+		c1 = inscr.createCompetition("Poker", datecloture, false);
+		c2 = inscr.createCompetition("Billard", datecloture2, true);
+		p1 = inscr.createPersonne("Robert","DeNiro","deniro.Robert@youtalkingtome.com");
+		p2 = inscr.createPersonne("Marlon","Brando","marlon.brando@thgodfhater.com");
+		e1 = inscr.createEquipe("Gangs");
+		e2 = inscr.createEquipe("Bulls");
+				
+		// Edite et test la dateCloture
+		c1.setDateCloture(LocalDate.of(2033, 04, 29));
+		assertEquals(LocalDate.of(2033, 04, 29),c1.getDateCloture());
+		// Test inscriptionsOuvertes
+		assertTrue(c1.inscriptionsOuvertes());
+		assertFalse(c2.inscriptionsOuvertes());
+		// Test addPersonne et removePersonne
+		c1.add(p1);
+		assertTrue(c1.getCandidats().contains(p1));
+		c1.remove(p1);
+		assertFalse(c1.getCandidats().contains(p1));
+		// Test estEnEquipe
+		assertTrue(c2.estEnEquipe());
+		assertFalse(c1.estEnEquipe());
+	
+	}
+
 	@Test
-	public void setUp() throws Exception {
-	
-	Inscriptions inscr = Inscriptions.getInscriptions();
+	public void testSetNom() {
+		c2.setNom("Boxe");
+		assertEquals("Boxe", c2.getNom());
+		assertEquals("Poker", c1.getNom());
+	}
 
-	Competition c1 = inscr.createCompetition("Poker", datecloture, false);
-	Competition c2 = inscr.createCompetition("Billard", datecloture2, true);
+	@Ignore
+	@Test
+	public void test2() {
+		
+	}
 	
-	Personne p1 = inscr.createPersonne("Robert","DeNiro","deniro.Robert@youtalkingtome.com");
-	Personne p2 = inscr.createPersonne("Marlon","Brando","marlon.brando@thgodfhater.com");
-
-	Equipe e1 = inscr.createEquipe("Gangs");
-	Equipe e2 = inscr.createEquipe("Bulls");
+	@Ignore
+	@Test
+	public void test3() {
+		
+	}
 	
-	// Edite et test le nom d'une competition
-	c2.setNom("Boxe");
-	assertEquals("Poker", c1.getNom());
-	assertEquals("Boxe", c2.getNom());
-	// Edite et test la dateCloture
-	c1.setDateCloture(LocalDate.of(2033, 04, 29));
-	assertEquals(LocalDate.of(2033, 04, 29),c1.getDateCloture());
-	// Test inscriptionsOuvertes
-	assertTrue(c1.inscriptionsOuvertes());
-	assertFalse(c2.inscriptionsOuvertes());
-	// Test addPersonne et removePersonne
-	c1.add(p1);
-	assertTrue(c1.getCandidats().contains(p1));
-	c1.remove(p1);
-	assertFalse(c1.getCandidats().contains(p1));
-	// Test estEnEquipe
-	assertTrue(c2.estEnEquipe());
-	assertFalse(c1.estEnEquipe());
-	
+	@Ignore
+	@Test
+	public void test4() {
+		
 	}
 }
