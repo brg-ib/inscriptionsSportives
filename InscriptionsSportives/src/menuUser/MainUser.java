12b54c8f9d5363ead0e684b5845701433a6425ec
Dina <dina.bel1999@gmail.com>
2019-01-31 22:11:50 +0100

    menu du Candidat

--- InscriptionsSportives/src/menuUser/MainUser.java
+++ InscriptionsSportives/src/menuUser/MainUser.java
@@ -1,69 +1,171 @@
 package menuUser;
 
 import java.util.*;
-
+import java.util.ArrayList;
 import inscriptions.*;
 import commandLineMenus.Action;
+import commandLineMenus.List;
+import commandLineMenus.ListData;
+import commandLineMenus.ListOption;
 import commandLineMenus.Menu;
 import commandLineMenus.Option;
+import commandLineMenus.examples.ListOptions;
 import commandLineMenus.rendering.examples.util.InOut;
 
 public class MainUser {
 
-
+	
 	// Menu principal
 	 private static Menu getMainMenu(){
 		Menu menu = new Menu("Inscriptions Sportives");
-		menu.add(menuInscriptions());
-		menu.add(menuCandidats());
-		menu.add(menuCompetitions());
+//		menu.add(getmenuInscriptions());
+		menu.add(getmenuCandidats());
+//		menu.add(getmenuCompetitions());
+//		menu.add(getmenuPersonne());
+//		menu.add(getmenuExit());
 		menu.addQuit("q");
 		return menu;
 	}
 
-	private Menu menuExit(){
-		Menu quitter = new Menu("Quitter", "q");
-		quitter.addQuit("q");
-		return quitter;
-	}
-	
-	
-	// Menu Inscriptions
-	private static Menu menuInscriptions() {
-		Menu menu = new Menu("Inscriptions", "i");
-		// TODO
-		
-		return menu;
-	}
-	// TODO
 
+
+
+	
+//	DINA
+	 
 	
 	// Menu Candidats
-	private static Menu menuCandidats() {
-		Menu menu = new Menu("Candidats", "c");
+	private static Menu getmenuCandidats() {
+		Menu menu = new Menu("Candidats", "Candidat", "C");
 		// TODO
-		
+		menu.add(getCandidatsOption());
+		menu.add(getCandidatsOption1());
+		menu.add(getCandidatsOption2());
+		menu.add(getCandidatsOption3());
+		menu.addBack("B");
+		menu.setAutoBack(true);
 		return menu;
 	}
+	
+	static Option getCandidatsOption()
+	{
+		return new Option("Nom", "N", getCandidatsNom());
+
+	}
+	
+	static Option getCandidatsOption1()
+	{
+
+		return new Option("Delete", "D", CandidatsDelete());
+
+	}
+	
+	static Option getCandidatsOption2()
+	{
+
+		return new Option("Modifier le Nom", "MN", setCandidatsNom());
+	}
+	
+	static Option getCandidatsOption3()
+	{
+
+		return new Option("Compétitions", "C", getCandidatsCompetitions());
+
+	}
+	
+	static ArrayList<String> al = new ArrayList<String>();
+
+	
+	static Action getCandidatsNom()
+	{
+		return new Action()
+		{
+			public void optionSelected()
+			{
+//				System.out.println("Marcel");
+				al.add("Ginette");
+				
+			      System.out.println("Nom du candidat : " + al);
+			    
+			}
+		};
+	}
+
+	
+	static Action CandidatsDelete()
+	{
+		return new Action()
+		{
+			public void optionSelected()
+			{
+				
+				al.add("Ginette");
+				al.remove("Ginette");
+				System.out.println(al + " has been deleted.");
+				
+			}
+		};
+	}
+		
+	static Action getCandidatsCompetitions()
+	{
+		return new Action()
+		{
+			public void optionSelected()
+			{
+				System.out.println("Basket");
+				System.out.println("Golf");
+				System.out.println("Course de cheveaux");
+				System.out.println("Handball");
+			}
+		};
+	}
+	
+	static Action setCandidatsNom()
+	{
+		return new Action()
+		{
+			public void optionSelected()
+			{
+				Scanner sc = new Scanner(System.in);
+				int i = sc.nextInt();
+				String n = sc.nextLine();
+				al.set(i, n);
+			}
+		};
+	}
+	
 	
 	// TODO
 
 	
-	// Menu Competitions
-	private static Menu menuCompetitions() {
-		Menu menu = new Menu("Competitions", "p");
-		// TODO
-		
-		return menu;
-	}
+//	 Menu Competitions
+//	private static Menu getmenuCompetitions() {
+//		Menu menu = new Menu("Competitions", "cp");
+//		// TODO
+//		
+//		return menu;
+//	}
 	// TODO
+
+	
+	// Menu Personne
+//		private static Menu getmenuPersonne() {
+//			Menu menu = new Menu("Personnes", "p");
+//			// TODO
+//			
+//			return menu;
+//		}
+		// TODO
 
 	
 	// Main
 	public static void main(String[] args)
 	{
-		Menu rootMenu = getMainMenu();
-		rootMenu.start();
+		Menu menu = getMainMenu();
+		menu.start();
+		
+	
 	}
 	
 	
