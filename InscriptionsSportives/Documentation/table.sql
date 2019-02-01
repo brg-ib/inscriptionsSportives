6b83b3d51213df476998d6088265b2956480abcc
brg-ib <ihcen@me.com>
2019-02-01 16:48:33 +0100

    MaJ BDD + MCD

--- InscriptionsSportives/Documentation/table.sql
+++ InscriptionsSportives/Documentation/table.sql
@@ -4,6 +4,48 @@
 
 
 #------------------------------------------------------------
+# Table: Candidat
+#------------------------------------------------------------
+
+CREATE TABLE Candidat(
+        id  Int  Auto_increment  NOT NULL ,
+        nom Varchar (50) NOT NULL
+	,CONSTRAINT Candidat_PK PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Personne
+#------------------------------------------------------------
+
+CREATE TABLE Personne(
+        id_Candidat Int NOT NULL ,
+        id          Int NOT NULL ,
+        Prenom      Varchar (50) NOT NULL ,
+        Mail        Varchar (50) NOT NULL ,
+        nom         Varchar (50) NOT NULL
+	,CONSTRAINT Personne_PK PRIMARY KEY (id_Candidat,id)
+
+	,CONSTRAINT Personne_Candidat_FK FOREIGN KEY (id_Candidat) REFERENCES Candidat(id)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Equipe
+#------------------------------------------------------------
+
+CREATE TABLE Equipe(
+        id_Candidat  Int NOT NULL ,
+        id           Int NOT NULL ,
+        Nom          Varchar (50) NOT NULL ,
+        nom_Candidat Varchar (50) NOT NULL
+	,CONSTRAINT Equipe_PK PRIMARY KEY (id_Candidat,id)
+
+	,CONSTRAINT Equipe_Candidat_FK FOREIGN KEY (id_Candidat) REFERENCES Candidat(id)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
 # Table: Competition
 #------------------------------------------------------------
 
@@ -13,52 +55,6 @@ CREATE TABLE Competition(
         Date_cloture Date NOT NULL ,
         EnEquipe     Bool NOT NULL
 	,CONSTRAINT Competition_PK PRIMARY KEY (id)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Candidat
-#------------------------------------------------------------
-
-CREATE TABLE Candidat(
-        id             Int  Auto_increment  NOT NULL ,
-        id_Competition Int NOT NULL
-	,CONSTRAINT Candidat_PK PRIMARY KEY (id)
-
-	,CONSTRAINT Candidat_Competition_FK FOREIGN KEY (id_Competition) REFERENCES Competition(id)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Personne
-#------------------------------------------------------------
-
-CREATE TABLE Personne(
-        id_Candidat    Int NOT NULL ,
-        id             Int NOT NULL ,
-        Prenom         Varchar (50) NOT NULL ,
-        Mail           Varchar (50) NOT NULL ,
-        id_Competition Int NOT NULL
-	,CONSTRAINT Personne_PK PRIMARY KEY (id_Candidat,id)
-
-	,CONSTRAINT Personne_Candidat_FK FOREIGN KEY (id_Candidat) REFERENCES Candidat(id)
-	,CONSTRAINT Personne_Competition0_FK FOREIGN KEY (id_Competition) REFERENCES Competition(id)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Equipe
-#------------------------------------------------------------
-
-CREATE TABLE Equipe(
-        id_Candidat    Int NOT NULL ,
-        id             Int NOT NULL ,
-        Nom            Varchar (50) NOT NULL ,
-        id_Competition Int NOT NULL
-	,CONSTRAINT Equipe_PK PRIMARY KEY (id_Candidat,id)
-
-	,CONSTRAINT Equipe_Candidat_FK FOREIGN KEY (id_Candidat) REFERENCES Candidat(id)
-	,CONSTRAINT Equipe_Competition0_FK FOREIGN KEY (id_Competition) REFERENCES Competition(id)
 )ENGINE=InnoDB;
 
 
@@ -75,5 +71,19 @@ CREATE TABLE APPARTENIR(
 
 	,CONSTRAINT APPARTENIR_Personne_FK FOREIGN KEY (id_Candidat,id) REFERENCES Personne(id_Candidat,id)
 	,CONSTRAINT APPARTENIR_Equipe0_FK FOREIGN KEY (id_Candidat_Equipe,id_Equipe) REFERENCES Equipe(id_Candidat,id)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: INSCRIRE
+#------------------------------------------------------------
+
+CREATE TABLE INSCRIRE(
+        id          Int NOT NULL ,
+        id_Candidat Int NOT NULL
+	,CONSTRAINT INSCRIRE_PK PRIMARY KEY (id,id_Candidat)
+
+	,CONSTRAINT INSCRIRE_Competition_FK FOREIGN KEY (id) REFERENCES Competition(id)
+	,CONSTRAINT INSCRIRE_Candidat0_FK FOREIGN KEY (id_Candidat) REFERENCES Candidat(id)
 )ENGINE=InnoDB;
 
