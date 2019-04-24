0ce36e65b1e2860de4ae8520839f45eee5392748
brg-ib <ihcen@me.com>
2019-04-24 13:59:03 +0200

    Ajout config hibernate

--- src/inscriptions/Personne.java
+++ src/inscriptions/Personne.java
@@ -4,14 +4,30 @@ import java.util.Collections;
 import java.util.Set;
 import java.util.TreeSet;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+import org.hibernate.annotations.SortNatural;
+
+import hibernate.Passerelle;
 /**
  * Représente une personne physique pouvant s'inscrire à une compétition.
  */
 
+@Entity
 public class Personne extends Candidat
 {
 	private static final long serialVersionUID = 4434646724271327254L;
 	private String prenom, mail;
+	
+	@OneToMany(targetEntity=Personne.class, mappedBy = "equipes", fetch=FetchType.EAGER)
+	@Cascade(value= { CascadeType.ALL })
+	
+	@SortNatural
 	private Set<Equipe> equipes;
 	
 	Personne(Inscriptions inscriptions, String nom, String prenom, String mail)
@@ -74,11 +90,15 @@ public class Personne extends Candidat
 	
 	boolean add(Equipe equipe)
 	{
+		equipes.add(equipe);
+		Passerelle.save(equipe);
 		return equipes.add(equipe);
 	}
 
 	boolean remove(Equipe equipe)
 	{
+		equipes.remove(equipe);
+		Passerelle.delete(equipe);
 		return equipes.remove(equipe);
 	}
 	
