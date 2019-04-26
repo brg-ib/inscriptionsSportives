1dacea93d44301fc9739187d4a11d37812c1cce2
brg-ib <ihcen@me.com>
2019-04-26 13:55:52 +0200

    add class hibernate

--- src/hibernate/Main.java
+++ src/hibernate/Main.java
@@ -1,10 +1,11 @@
 package hibernate;
 
-public class Main {
+import hibernate.Passerelle;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Main
+{
+	public static void main(String[] args)
+	{
+		new Passerelle();
 	}
-
 }
